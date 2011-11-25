a20086a85ce5c4046f5f461dddb525a91649758b
Jorge Diaz <jorge@fractalmedia.mx>
2011-11-24 18:02:54 -0600

    primer comit para web

---
+++ src/web.clj
@@ -0,0 +1,22 @@
+(ns web)
+
+
+(defn table 
+  ([coll]
+   [:table
+    (for [row coll]
+      [:tr
+       (map (fn [elem] [:td elem]) row)])])
+  ([titles coll]
+   (table (cons titles coll))))
+
+(defn link-to [obj reference]
+  [:a {href reference} obj])
+
+(defn bullet-list [coll]
+  [:ul
+   (map (fn [elem] [:li elem]))])
+
+(defn number-list [coll]
+  [:ol
+   (map (fn [elem] [:li elem]))])
