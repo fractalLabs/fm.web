2944632ada0595cab7fbddc375a956cbe02dafd4
nex <nex@nex-lap2.(none)>
2012-08-05 23:30:41 -0500

    ahora es fm.web, en vez de web.core

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject web "1.0.0-SNAPSHOT"
+(defproject fm.web "1.0.0-SNAPSHOT"
   :description "lib de htmp con sintaxis de hiccup"
   :dependencies [[org.clojure/clojure "1.2.0"]])
---
+++ src/fm/web.clj
@@ -0,0 +1,19 @@
+(ns fm.web)
+
+
+(defn table 
+  ([options coll]
+   [:table options
+    (for [row coll]
+      [:tr
+       (map (fn [elem] [:td elem]) row)])])
+  ([coll]
+   (table coll {})))
+
+(defn bullet-list [coll]
+  [:ul
+   (map (fn [elem] [:li elem]) coll)])
+
+(defn number-list [coll]
+  [:ol
+   (map (fn [elem] [:li elem]) coll)])
