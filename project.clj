76afe918b40fe6d12978c1428361c4ac8288f36f
nex <nex@pgr.lan>
2012-09-19 00:42:19 -0500

    borre basura, clj 1.4

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject fm.web "1.0.0-SNAPSHOT"
   :description "lib de htmp con sintaxis de hiccup"
-  :dependencies [[org.clojure/clojure "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
